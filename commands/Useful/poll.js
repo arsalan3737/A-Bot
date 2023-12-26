0fb01ac65ad1321ab40239db0913943a67d4f8a8
arsalan3737 <arsalan373737@gmail.com>
2023-12-26 01:20:28 -0600

    added more options

--- commands/Useful/poll.js
+++ commands/Useful/poll.js
@@ -30,16 +30,33 @@ module.exports = {
         .setName(`option2`)
         .setDescription(`Used to change the second option`)
         .setRequired(false)
+    )
+    .addStringOption((option) =>
+      option
+        .setName(`option3`)
+        .setDescription(`Used to change the third option`)
+        .setRequired(false)
+    )
+    .addStringOption((option) =>
+      option
+        .setName(`option4`)
+        .setDescription(`Used to change the fourth option`)
+        .setRequired(false)
     ),
   async execute(interaction) {
     let option1 = interaction.options.getString("option1")
       ? interaction.options.getString("option1")
-      : `yes`;
+      : `Yes`;
     let option2 = interaction.options.getString("option2")
       ? interaction.options.getString("option2")
       : `No`;
+    let option3 = interaction.options.getString("option3");
+    let option4 = interaction.options.getString("option4");
+
     let yes = 0;
     let no = 0;
+    let o3 = 0;
+    let o4 = 0;
     const votedUser = new Set();
     const question = interaction.options.getString("question");
     const yButton = new ButtonBuilder()
@@ -47,7 +64,7 @@ module.exports = {
       .setCustomId(`yes`)
       .setLabel(option1);
     const nButton = new ButtonBuilder()
-      .setStyle(ButtonStyle.Danger)
+      .setStyle(ButtonStyle.Success)
       .setCustomId(`no`)
       .setLabel(option2);
     const but = new ActionRowBuilder().addComponents(yButton, nButton);
@@ -57,6 +74,25 @@ module.exports = {
         { name: option1, value: `${yes}`, inline: true },
         { name: option2, value: `${no}`, inline: true }
       );
+    if (option3 != null) {
+      const thirdButton = new ButtonBuilder()
+        .setStyle(ButtonStyle.Success)
+        .setCustomId(`thirdOpt`)
+        .setLabel(option3);
+      console.log(option3);
+      but.addComponents(thirdButton);
+      embed.addFields({ name: option3, value: `${o3}`, inline: true });
+    }
+    if (option4 != null) {
+      const fourthButton = new ButtonBuilder()
+        .setStyle(ButtonStyle.Success)
+        .setCustomId(`fourthOpt`)
+        .setLabel(option4);
+      console.log(option4);
+      but.addComponents(fourthButton);
+      embed.addFields({ name: option4, value: `${o4}`, inline: true });
+    }
+
     const response = await interaction.reply({
       embeds: [embed],
       components: [but],
@@ -76,12 +112,20 @@ module.exports = {
       votedUser.add(interaction.user.id);
       if (interaction.customId === `yes`) yes++;
       if (interaction.customId === `no`) no++;
+      if (interaction.customId === `thirdOpt`) o3++;
+      if (interaction.customId === `fourthOpt`) o4++;
       let embed2 = new EmbedBuilder()
         .setTitle(question)
         .addFields(
           { name: option1, value: `${yes}`, inline: true },
           { name: option2, value: `${no}`, inline: true }
         );
+      if (option3 != null) {
+        embed2.addFields({ name: option3, value: `${o3}`, inline: true });
+      }
+      if (option4 != null) {
+        embed2.addFields({ name: option4, value: `${o4}`, inline: true });
+      }
       const response = await interaction.update({
         embeds: [embed2],
         components: [but],
